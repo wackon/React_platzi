a671fbc787e89cea7f064d00eb08c7a0dd0d7404
wackon <jeac86@gmail.com>
2020-07-06 17:39:04 -0500

    cap 7

--- src/index.js
+++ src/index.js
@@ -4,15 +4,39 @@
 
 // const container = document.getElementById('app')
 
-
 // container.appendChild(element)
 
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from "react";
+import ReactDom from "react-dom";
 
-const element = <h1>Hello Platzi Badges from React!</h1>
+// const jsx = <h1>Hello Platzi Badges from React!</h1>;
+// const element = React.createElement(
+//   "a",
+//   { href: "http://platzi.com" },
+//   "Ir a platzi"
+// );
 
-const container = document.getElementById('app')
+// const name = "Camilo";
+// const suma = () => 3 + 3
+// // const element = React.createElement("h1", {}, `Hola soy ${name}`);
+// const jsx = <h1>'Hola! soy ,{undefined}</h1>
+
+// const jsx = <div>
+//     <h1>
+//         Hola, soy Richard
+//     </h1>
+//     <p>
+//         Soy ingeniero frontend
+//     </p>
+// </div>
+
+const element = React.createElement(
+  "div",
+  {},
+  React.createElement("h1", {}, "Hola soy Richard"),
+  React.createElement("p", {}, "Soy ingeniero de la Web")
+);
+const container = document.getElementById("app");
 
 // ReactDom.render(__qué,____Donde_____)
-ReactDom.render(element, container)
+ReactDom.render(element, container);
