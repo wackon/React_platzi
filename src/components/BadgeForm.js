215b2b192d7b419b2d0a3a83f77cdb2ac46368e2
wackon <jeac86@gmail.com>
2020-07-15 12:33:38 -0500

    continuacion formulario y trabajo state

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,8 +1,16 @@
 import React from "react";
 
 class BadgeForm extends React.Component {
+  state = {
+    jobTitle: "Designer",
+  };
+
   handleChange = (e) => {
-    console.log({ name: e.target.name, value: e.target.value });
+    //console.log({ name: e.target.name, value: e.target.value });
+
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
   };
 
   handleClick = (e) => {
@@ -11,7 +19,8 @@ class BadgeForm extends React.Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log("form was submit");
+    console.log("form was submitted");
+    console.log(this.state);
   };
 
   render() {
@@ -28,8 +37,58 @@ class BadgeForm extends React.Component {
               type="text"
               placeholder="Primer Nombre"
               name="firstName"
+              value={this.state.firstName}
             />
           </div>
+
+          <div className="form-group">
+            <label>Last Name</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              placeholder="Segundo Nombre"
+              name="lastName"
+              value={this.state.lastName}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="email"
+              placeholder="Correo Electronico"
+              name="email"
+              value={this.state.email}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Job Title</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              placeholder="Cargo"
+              name="jobTitle"
+              value={this.state.jobTitle}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>LinkedIn</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              placeholder="LinkedIn"
+              name="linkedin"
+              value={this.state.linkedin}
+            />
+          </div>
+
           <button
             /*Type="button"*/ onClick={this.handleClick}
             className="btn btn-primary"
