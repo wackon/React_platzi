3b4f1dfe3864f33ce72fc51245a37b4866a8f55d
wackon <jeac86@gmail.com>
2020-07-15 15:09:11 -0500

    cap 16 compartir informacion a otros componentes

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,17 +1,15 @@
 import React from "react";
 
 class BadgeForm extends React.Component {
-  state = {
-    jobTitle: "Designer",
-  };
+  // state = {};
 
-  handleChange = (e) => {
-    //console.log({ name: e.target.name, value: e.target.value });
+  // handleChange = (e) => {
+  //   //console.log({ name: e.target.name, value: e.target.value });
 
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  };
+  //   this.setState({
+  //     [e.target.name]: e.target.value,
+  //   });
+  // };
 
   handleClick = (e) => {
     console.log("button was clicked");
@@ -32,60 +30,60 @@ class BadgeForm extends React.Component {
           <div className="form-group">
             <label>First Name</label>
             <input
-              onChange={this.handleChange}
+              onChange={this.props.onChange}
               className="form-control"
               type="text"
               placeholder="Primer Nombre"
               name="firstName"
-              value={this.state.firstName}
+              value={this.props.formValues.firstName}
             />
           </div>
 
           <div className="form-group">
             <label>Last Name</label>
             <input
-              onChange={this.handleChange}
+              onChange={this.props.onChange}
               className="form-control"
               type="text"
               placeholder="Segundo Nombre"
               name="lastName"
-              value={this.state.lastName}
+              value={this.props.formValues.lastName}
             />
           </div>
 
           <div className="form-group">
             <label>Email</label>
             <input
-              onChange={this.handleChange}
+              onChange={this.props.onChange}
               className="form-control"
               type="email"
               placeholder="Correo Electronico"
               name="email"
-              value={this.state.email}
+              value={this.props.formValues.email}
             />
           </div>
 
           <div className="form-group">
             <label>Job Title</label>
             <input
-              onChange={this.handleChange}
+              onChange={this.props.onChange}
               className="form-control"
               type="text"
               placeholder="Cargo"
               name="jobTitle"
-              value={this.state.jobTitle}
+              value={this.props.formValues.jobTitle}
             />
           </div>
 
           <div className="form-group">
             <label>LinkedIn</label>
             <input
-              onChange={this.handleChange}
+              onChange={this.props.onChange}
               className="form-control"
               type="text"
               placeholder="LinkedIn"
               name="linkedin"
-              value={this.state.linkedin}
+              value={this.props.formValues.linkedin}
             />
           </div>
 
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -6,6 +6,27 @@ import Badge from "../components/Badge";
 import BadgeForm from "../components/BadgeForm";
 
 class BadgeNew extends React.Component {
+  state = {
+    form: {
+      fistName: "",
+      lastName: "",
+      email: "",
+      jobTitle: "",
+      linkedin: "",
+    },
+  };
+
+  handleChange = (e) => {
+    //const nexForm = this.state.form;
+    //nextForm[e.target.name] = e.target.value
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
   render() {
     return (
       <div>
@@ -17,16 +38,20 @@ class BadgeNew extends React.Component {
           <div className="row">
             <div className="col-6">
               <Badge
-                firstName="Jorge"
-                lastName="Alvarez"
-                email="jeac86@gmail.com"
-                jobTitle="Frontnd Engineer "
+                firstName={this.state.form.firstName}
+                lastName={this.state.form.lastName}
+                email={this.state.form.email}
+                jobTitle={this.state.form.jobTitle}
+                linkedid={this.state.form.linkedin}
                 avatarUrl="https://gravatar.com/avatar/ee51eddea6dee1a8b8da84a175833caf?s=400&d=robohash&r=x"
               />
             </div>
 
             <div className="col-6">
-              <BadgeForm />
+              <BadgeForm
+                onChange={this.handleChange}
+                formValues={this.state.form}
+              />
             </div>
           </div>
         </div>
